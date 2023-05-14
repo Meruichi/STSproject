23c679636e088ae474932f26f919c301f1f0b64b
Meruichi <lsh12031237@naver.com>
2023-05-14 16:27:06 +0900

    docs(Controller)

--- yoyang/src/main/java/com/meruichi/yoyang/controller/api/BranchApiController.java
+++ yoyang/src/main/java/com/meruichi/yoyang/controller/api/BranchApiController.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.meruichi.yoyang.config.auth.PrincipalDetail;
 import com.meruichi.yoyang.dto.ResponseDto;
 import com.meruichi.yoyang.model.Branch;
--- yoyang/src/main/java/com/meruichi/yoyang/controller/api/ItemReportApiController.java
+++ yoyang/src/main/java/com/meruichi/yoyang/controller/api/ItemReportApiController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.meruichi.yoyang.config.auth.PrincipalDetail;
 import com.meruichi.yoyang.dto.ResponseDto;
 import com.meruichi.yoyang.model.ItemReport;
